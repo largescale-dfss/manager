4d50e3339c94be1d1c8d3b277af9636744b8d97d
imalan <alanchen@nyu.edu>
2015-12-18 09:26:41 -0800

    datanode protobuf files

--- run_codegen.sh
+++ run_codegen.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 protoc -I . --python_out=. --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_python_plugin` namenode.proto
+protoc -I . --python_out=. --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_python_plugin` datanode.proto
