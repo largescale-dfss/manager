e728cb2238b9db9e12e97658927c96657c6e7866
imalan <alanchen@nyu.edu>
2015-12-18 19:06:42 -0800

    added virtual environment and rewrote scripts

--- run_datanode.sh
+++ run_datanode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 read -p "Please enter port number for datanode: " port
-/home/cs202/Desktop/grpc/python2.7_virtual_environment/bin/python ./rdatanode.py $port
+./venv/bin/python ./rdatanode.py $port
 
 
--- run_namenode.sh
+++ run_namenode.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 read -p "Please enter the port number: " port
-/home/cs202/Desktop/grpc/python2.7_virtual_environment/bin/python ./rnamenode.py $port
+./venv/bin/python ./rnamenode.py $port
 
